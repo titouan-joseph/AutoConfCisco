66e96777f2a3e3ad01fd0e51457f3f549905bdba
titouan-joseph <titouanjoseph@gmail.com>
2021-01-11 09:26:30 +0100

    feat(Push configuration): :wrestling: Multithread and arguments
    :heavy_plus_sign: one thread for one router
    :heavy_plus_sign: argparse
    to confugure one router or one protocol
    
    Closes #1
    Closes #3

---
+++ main_args.py
@@ -0,0 +1,120 @@
+import argparse
+import threading
+import util
+import json
+import socket
+
+
+def pushOne(configurator, config, arguments, confugure_all):
+    # Enter in router
+    configurator.globalConfigMode()
+
+    # Change hostname
+    configurator.changeHostname()
+
+    # Activated IPv6
+    configurator.activeIPv6()
+
+    # OSPF
+    if arguments.ospf or confugure_all:
+        # Set OSPF
+        if "OSPF_id" in config:
+            configurator.setOSPFv2(config["OSPF_id"])
+            configurator.setOSPFv3(config["OSPF_id"])
+
+        # Set OSPF neighbour
+        if "OSPF_neighbour" in config:
+            configurator.setNeighbourOSPFv2(config["OSPF_neighbour"])
+
+        # Set BGP neighbor
+        if "BGP" in config:
+            BGP_conf = config["BGP"]
+            configurator.setMPBGPneighborIPv4(BGP_conf["AS"],
+                                              BGP_conf["my_networks"],
+                                              BGP_conf["neighbor"])
+
+        for interface in config["interfaces"]:
+            if "OSPF_area" in interface:
+                configurator.activeOSPFv3Interface(interface["interfaceName"],
+                                             interface["OSPF_area"])
+
+    # MPLS
+    if arguments.mpls or confugure_all:
+        # Activated MPLS
+        if "ipcef" in config:
+            configurator.activeIPcef()
+        for interface in config["interfaces"]:
+            if "MPLS" in interface:
+                configurator.activeMPLSonInterface(interface["interfaceName"])
+
+    # Interface
+    if arguments.interface:
+        for interface in config["interfaces"]:
+            if interface["interfaceName"] != arguments.interface:
+                continue
+            if "IPv4" in interface:
+                configurator.setUpIPv4(interface["interfaceName"],
+                                       interface["IPv4"])
+
+            if "IPv6" in interface:
+                configurator.setUpIPv6(interface["interfaceName"],
+                                       interface["IPv6"])
+
+
+if __name__ == '__main__':
+    # var
+    threadList = []
+    all_protocol = False
+
+    # parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("jsonFile", help="The json configuration file")
+    parser.add_argument("--router", "-r", help="Configure only the router")
+    parser.add_argument("--ospf", "-o", help="Configure only OSPF", action='store_true')
+    parser.add_argument("--bgp", "-b", help="Configure only BGP", action='store_true')
+    parser.add_argument("--mpls", "-m", help="Configure only MPLS", action='store_true')
+    parser.add_argument("--write", "-w", help="Write configuration att the end", action='store_true')
+    parser.add_argument("--interface", "--int", "-i", help="Configure only one interface, or all interface with <all>")
+    args = parser.parse_args()
+
+    if not args.ospf and not args.bgp and not args.mpls:
+        all_protocol = True
+
+    with open(args.jsonFile, "r") as file:
+        conf = json.load(file)
+
+    for routerName, routerConf in conf.items():
+        if args.router:
+            if routerName != args.router:
+                continue
+
+        try:
+            routerManagementAddr = routerConf["IPaddr"]
+        except KeyError:
+            print(f"No IP address for {routerName}")
+            continue
+        try:
+            routerManagementPort = routerConf["port"]
+        except KeyError:
+            print(f"No port for {routerName}")
+            continue
+
+        routerSocket = socket.socket()
+        try:
+            routerSocket.connect((routerManagementAddr, routerManagementPort))
+            print(f"connect to {routerName}")
+        except ConnectionError:
+            print(f"Can't connect to {routerName}")
+
+        oneConfig = util.Configuration(routerSocket, routerName)
+
+        thread = threading.Thread(target=pushOne,
+                                  args=(oneConfig,
+                                        routerConf,
+                                        args,
+                                        all_protocol))
+        threadList.append(thread)
+        thread.start()
+
+for th in threadList:
+    th.join()
--- util.py
+++ util.py
@@ -12,7 +12,7 @@ class Configuration:
         time.sleep(0.1)  # Petite pause sinon on envoie les commandes trop vite
 
     def writeConfig(self):
-        print(f"{self.name} : Write configuration")
+        print(f"{self.name} : Write configuration \n")
         self.sendCommand("end")  # On évite d'etre dans un mode non voulu
         self.sendCommand("write")
         self.sendCommand("")  # Pour envoyer un enter et confirmer l'écriture du fichier
@@ -27,7 +27,7 @@ class Configuration:
 
     def changeHostname(self):
         self.configureTerminal()
-        print(f"{self.name} : Change hostname")
+        print(f"{self.name} : Change hostname \n")
         self.sendCommand(f"hostname {self.name}")
         self.sendCommand("end")
 
@@ -40,70 +40,70 @@ class Configuration:
         self.sendCommand("end")
 
     def setUpIPv4(self, interface, IPv4):
-        print(f"{self.name} : Configure {interface} with {IPv4}")
+        print(f"{self.name} : Configure {interface} with {IPv4} \n")
         self.interInInterfaceMode(interface)
         self.sendCommand(f"ip add {IPv4[0]} {IPv4[1]}")
         self.saveExitInterfaceMode()
 
     def setUpIPv6(self, interface, IPv6):
-        print(f"{self.name} : Configure {interface} with {IPv6}")
+        print(f"{self.name} : Configure {interface} with {IPv6} \n")
         self.interInInterfaceMode(interface)
         self.sendCommand(f"ipv6 add {IPv6}")
         self.saveExitInterfaceMode()
 
     def activeIPv6(self):
-        print(f"{self.name} : Enable IPv6")
+        print(f"{self.name} : Enable IPv6 \n")
         self.configureTerminal()
         self.sendCommand("ipv6 unicast-routing")
 
     def setOSPFv2(self, OSFP_id):
-        print(f"{self.name} : set OSFPv2")
+        print(f"{self.name} : set OSFPv2 \n")
         self.configureTerminal()
         self.sendCommand("router ospf 1")
         self.sendCommand(f"router-id {OSFP_id}")
 
     def setOSPFv3(self, OSPF_id):
-        print(f"{self.name} : set OSPFv3")
+        print(f"{self.name} : set OSPFv3 \n")
         self.configureTerminal()
         self.sendCommand("ipv6 router ospf 2")
         self.sendCommand(f"router-id {OSPF_id}")
 
     def setNeighbourOSPFv2(self, OSPF_neighbour):
-        print(f"{self.name} : set OSPFv2 neighbour")
+        print(f"{self.name} : set OSPFv2 neighbour \n")
         self.configureTerminal()
         self.sendCommand("router ospf 1")
         for neighbour in OSPF_neighbour:
             self.sendCommand(f"network {neighbour[0]} {neighbour[1]} area {neighbour[2]}")
 
     def activeOSPFv2Interface(self, interface, OSPF_area):
-        print(f"{self.name} : Active OSPFv2 on {interface}")
+        print(f"{self.name} : Active OSPFv2 on {interface} \n")
         self.interInInterfaceMode(interface)
         self.sendCommand(f"ip ospf 1 area {OSPF_area}")
         self.sendCommand("end")
 
     def activeOSPFv3Interface(self, interface, OSPF_area):
-        print(f"{self.name} : Active OSPFv3 on {interface}")
+        print(f"{self.name} : Active OSPFv3 on {interface} \n")
         self.interInInterfaceMode(interface)
         self.sendCommand(f"ipv6 ospf 2 area {OSPF_area}")
         self.sendCommand("end")
 
     def activeIPcef(self):
-        print(f"{self.name} : Active MPLS (ip cef)")
+        print(f"{self.name} : Active MPLS (ip cef) \n")
         self.configureTerminal()
         self.sendCommand("ip cef")
 
     def activeMPLSonInterface(self, interface):
-        print(f"{self.name} : Active MPLS on {interface}")
+        print(f"{self.name} : Active MPLS on {interface} \n")
         self.interInInterfaceMode(interface)
         self.sendCommand("mpls ip")
         self.sendCommand("end")
 
     def activeMPBGP(self, as_number):
-        print(f"{self.name} : active MPBGP")
+        print(f"{self.name} : active MPBGP \n")
         self.sendCommand(f"router bgp {as_number}")
 
     def setMPBGPneighborIPv4(self, as_number, my_networks, neighbor):
-        print(f"{self.name} : adding neighbor {neighbor} at AS : {as_number}")
+        print(f"{self.name} : adding neighbor {neighbor} at AS : {as_number} \n")
         self.activeMPBGP(as_number)
         for n in neighbor:
             self.sendCommand(f"neighbor {n['addr']} remote-as {n['AS']}")
